5cecfee9c6ab7d6a54fb114a1b1d39e4013c0244
Richard Michael <rmichael@edgeofthenet.org>
2013-04-10 20:03:08 +0200

    Write ruby stub functions.

--- ruby/encrypter/ext/encrypter/encrypter.c
+++ ruby/encrypter/ext/encrypter/encrypter.c
@@ -1,7 +1,12 @@
-#include <stdio.h>
+#include <ruby.h>
 
-int main()
+static VALUE encrypt(VALUE module)
 {
-  printf("Hello, World!\n");
-  return 0;
+  return rb_str_new2("Encrypted message.");
+}
+
+void Init_encrypter()
+{
+  VALUE mEncrypter = rb_define_module("Encrypter");
+  rb_define_singleton_method(mEncrypter, "encrypt", encrypt, 0);
 }
--- ruby/encrypter/ext/encrypter/extconf.rb
+++ ruby/encrypter/ext/encrypter/extconf.rb
@@ -1,2 +1,15 @@
 require 'mkmf'
+
+# TODO: This changes all the CFLAGS, as:
+# $ diff Makefile*
+# < CFLAGS   = -fno-common $(cflags)  -fno-common -pipe $(ARCH_FLAG)
+# ---
+# > CFLAGS   = -fno-common -Wall $(ARCH_FLAG)
+
+# with_cflags '-Wall' do
+#   create_makefile 'encrypter'
+# end
+
+# TODO: create_makefile 'encrypter/encrypter' # ==> @tenderlove's convention. Why? Creates install
+# point in a sub-directory.
 create_makefile 'encrypter'

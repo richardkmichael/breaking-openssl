84c9b9feceabf66941514a304ac333a7226bfdb8
Richard Michael <rmichael@edgeofthenet.org>
2013-04-13 14:46:34 +0200

    Renaming for clarity.

--- c/digest_example.c
+++ c/digest_example.c
@@ -14,14 +14,14 @@ int main(int argc, char* argv[])
   }
 
   // The encrypt function will fill in the message length.  Used to iterate later.
-  unsigned int encrypted_message_length_ptr;
+  unsigned int encrypted_message_length;
 
   // A pointer to the encrypted message.
-  unsigned char *encrypted_message_ptr = (unsigned char *)encrypt(message, &encrypted_message_length_ptr);
+  unsigned char *encrypted_message_ptr = (unsigned char *)encrypt(message, &encrypted_message_length);
 
   // Print out the encrypted message as 0-padded 2-digit unsigned hexidecimal.
   int i;
-  for(i = 0; i < encrypted_message_length_ptr; i++) {
+  for(i = 0; i < encrypted_message_length; i++) {
     // printf("%02x", encrypted_message_ptr[i]);  // Pointer / array equivalence.
     printf("%02x", encrypted_message_ptr[i]);  // Pointer / array equivalence.
   }
--- ruby/encrypter/ext/encrypter/encrypter.c
+++ ruby/encrypter/ext/encrypter/encrypter.c
@@ -1,13 +1,15 @@
 #include <encrypter.h>
 
+VALUE mEncrypter;
+
 // TODO: How to namespace functions in C; libencrypt has an encrypt() function too.
 static VALUE encrypter_encrypt(VALUE self, VALUE rb_string_plaintext)
 // static VALUE encrypter_encrypt()
 {
 
   // C90 mandates all declarations appear before code.
-  unsigned int encrypted_string_length_ptr;
-  char *plaintext_string, *encrypted_string;
+  unsigned int encrypted_string_length;
+  char *plaintext_string_ptr, *encrypted_string_ptr;
   VALUE rb_encrypted_string;
 
   // This will raise a TypeError exception; otherwise use "TYPE(rb_string) == T_STRING;".
@@ -22,18 +24,16 @@ static VALUE encrypter_encrypt(VALUE self, VALUE rb_string_plaintext)
   // StringValuePtr is not \0 terminated, use StringValueCStr (despite being slower) because encrypt
   // uses strlen() which requires \0 termination.
   // plaintext_string = StringValueCStr(rb_string_plaintext);
-  plaintext_string = RSTRING_PTR(rb_string_plaintext);
-  printf("DEBUG: plaintext_string: %s\n", plaintext_string);
+  plaintext_string_ptr = RSTRING_PTR(rb_string_plaintext);
+  printf("DEBUG: plaintext_string: %s\n", plaintext_string_ptr);
 
-  encrypted_string = encrypt(plaintext_string, &encrypted_string_length_ptr);
-  printf("DEBUG: encrypted_string: %s\n", encrypted_string);
+  encrypted_string_ptr = encrypt(plaintext_string_ptr, &encrypted_string_length);
+  printf("DEBUG: encrypted_string: %s\n", encrypted_string_ptr);
 
   // return rb_str_new2("Encrypted message.");
-  rb_encrypted_string = rb_str_new2(encrypted_string); // rb_str_new2 initializes from a signed char*.
+  rb_encrypted_string = rb_str_new2(encrypted_string_ptr); // rb_str_new2 initializes from a signed char*.
   return rb_encrypted_string;
 }
-
-VALUE mEncrypter;
 
 void Init_encrypter()
 {
